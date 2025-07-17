dff48f40124cde626160471a0305f7d7f99e2d3c
ashish2508 <jha250805@gmail.com>
2025-07-17 01:49:48 +0000

    feat: ✨ cors working, redirect working as well

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-import {Elysia} from 'elysia'
-import { cors } from '@elysiajs/cors'
-import multer from 'multer';
-import { v4 as uuidv4 } from 'uuid';
+import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
 
-new Elysia()
-    .get('/', () => 'Hello world')
-    .get('/user/:id', ({ params: { id }}) => id)
-    .post('/form', ({ body }) => body)
-    	.use(
-		cors({
-			origin: ["http://localhost:3000", "http://localhost:5173"],
-      credentials: true
-		})
-	)
-.use((ctx,next))=>{
-
-  }
+const app = new Elysia() 
+    .use(
+        cors({
+            origin: ["http://localhost:3000", "http://localhost:5173"],
+            credentials: true
+        })
+    )
+    .onRequest(({ set }) => {
+        set.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
+        set.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+        set.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+    })
+    .get('/', () => 'Hello World!')
+      .get('/ashishjha', () => {
+        return Response.redirect('https://ashishjha.tech', 302); 
+    })
     
-  )) 
-    .listen(8000)
+    .get('/user/:id', ({ params: { id } }) => id)
+    .post('/form', ({ body }) => body)
+    .onError(({ code }) => {
+        if (code === 'NOT_FOUND') return 'Route not found :('
+    })
+    .listen(8000);
 
+app.handle(new Request('http://localhost/')).then(console.log)
