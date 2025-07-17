2d3b45046799efb7b98924254a1aa4f57132ff8f
ashish2508 <jha250805@gmail.com>
2025-07-17 01:09:10 +0000

    feat: ✨added cors ?

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
 
 new Elysia()
-    .get('/', 'Hello world')
+    .get('/', () => 'Hello world')
     .get('/user/:id', ({ params: { id }}) => id)
     .post('/form', ({ body }) => body)
-    .use(cors({
+    	.use(
+		cors({
+			origin: ["http://localhost:3000", "http://localhost:5173"],
+      credentials: true
+		})
+	)
+.use((ctx,next))=>{
+
+  }
     
-  })) 
+  )) 
     .listen(8000)
 
