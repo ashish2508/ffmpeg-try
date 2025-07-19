d6663bfdf7340566546759e3cc88d0885ef40f6f
ashish2508 <jha250805@gmail.com>
2025-07-19 21:24:38 +0000

    style: 💅

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,32 +2,51 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import VideoPlayer from './VideoPlayer'
+import { useRef } from 'react'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const playerRef = useRef(null)
+  const videoLink = "http://localhost:8000/uploads/courses/dfd7c54d-e79b-4ff2-9578-c6209e5aa412/index.m3u8"
+  const videoPlayerOptions = {
+    controls: true,
+    autoplay: false,
+    fluid: true,
+    responsive: true,
+    sources: [{
+      src: videoLink,
+      type: 'application/x-mpegURL'
+    }]
+  }
+  const handlePlayerReady = (player)=>{
+    playerRef.current = player;
+    
+    //player events
+    player.on("waiting",()=>{
+      videojs.log("player is waiting")
+    })
+    player.on("dispose",()=>{
+      videojs.log("player will dispose")
+    });
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Video Player</h1>
+    </div>
+      <VideoPlayer
+        ref={playerRef}
+        options={{
+          autoplay: true,
+          controls: true,
+          sources: [{
+            src: videoLink,
+            type: 'application/x-mpegURL'
+          }]
+        }}
+        onReady={(player) => {
+          playerRef.current = player
+        }}
+      />
     </>
   )
 }
